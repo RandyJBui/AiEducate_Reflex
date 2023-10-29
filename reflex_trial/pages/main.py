94713f0856bf1f44e8f632bcb4f79360c5864b46
RandyJBui <randyjbui888@gmail.com>
2023-10-29 00:02:36 -0700

    pre-frontend

--- reflex_trial/pages/main.py
+++ reflex_trial/pages/main.py
@@ -4,6 +4,7 @@ from reflex_trial import styles
 from reflex_trial.state import State
 from credentials import *
 
+
 def leftright(a,q) ->rx.Component:
      return rx.box(
         rx.box(
@@ -16,34 +17,44 @@ def leftright(a,q) ->rx.Component:
         ),
         margin_y="1em",
     )
+
 @template(route="/", title="Practice AI")
+
 def main() -> rx.Component: 
 
-    return rx.hstack(rx.container(
-        rx.vstack( 
-             rx.container(
+    print("State.quizFlip == False")
+    return rx.container(rx.cond(State.quizFlip, showQuiz(),showMain()))
+
+def showQuiz() -> rx.Component():
+        return rx.container(
                 rx.vstack( 
-                        rx.foreach(State.hist, lambda x: leftright(x[0],x[1])), 
-                        rx.box(
-                                rx.box(rx.text(State.query, style=styles.question_style), text_align="right",),
-                                rx.box(rx.text(State.updates, style=styles.answer_style) ) ,text_align="left",),  
-                rx.hstack( 
-                    rx.vstack(
-                        rx.input(placeholder="Age", value=State.age, on_change=State.set_age),
-                        rx.input(placeholder="Prompt", value=State.query, on_change=State.set_query) ), 
-                rx.button("Go",color_scheme="green", on_click=State.generate()) ), style=styles.message_style,  )
-    ) ) ),
-      rx.container(
-        rx.vstack( 
-             rx.container(
-                rx.vstack( 
-                        rx.foreach(State.hist, lambda x: leftright(x[0],x[1])), 
-                        rx.box(
-                                rx.box(rx.text(State.query, style=styles.question_style), text_align="right",),
-                                rx.box(rx.text(State.updates, style=styles.answer_style) ) ,text_align="left",),  
-                rx.hstack( 
-                    rx.vstack(
-                        rx.input(placeholder="Age", value=State.age, on_change=State.set_age),
-                        rx.input(placeholder="Prompt", value=State.query, on_change=State.set_query) ), 
-                rx.button("Go",color_scheme="green", on_click=State.generate()) ), style=styles.message_style,  )
-    ) ) ) )
+                    rx.container(
+                        rx.vstack( 
+                                rx.foreach(State.quizList, lambda x: leftright(x[0],x[1]),), 
+                                rx.box(
+                                        rx.box(rx.text(State.answer, style=styles.question_style), text_align="right",),
+                                        rx.box(rx.text(State.quizPrompt, style=styles.answer_style) ) ,text_align="left",),  
+                        rx.hstack( 
+                                rx.vstack(rx.input(placeholder="Answer", value=State.answer, on_change=State.set_answer)), 
+                        rx.button("Go",color_scheme="green", on_click=State.quizClicked() ), style=styles.message_style, )
+            ) ) ) )
+
+def showMain() ->rx.Component:
+     return    rx.container(
+            rx.vstack( 
+                rx.container(
+                    rx.vstack( 
+                            rx.foreach(State.hist, lambda x: leftright(x[0],x[1]),), 
+                            rx.box(
+                                    rx.box(rx.text(State.query, style=styles.question_style), text_align="right",),
+                                    rx.box(rx.text(State.updates, style=styles.answer_style) ) ,text_align="left",),  
+                    rx.hstack( 
+                        rx.vstack( 
+                            rx.input(placeholder="Age", value=State.age, on_change=State.set_age),
+                            rx.input(placeholder="Prompt", value=State.query, on_change=State.set_query)) , 
+                    rx.button("Go",color_scheme="green", on_click=State.generate()) ), style=styles.message_style,  ) )
+        ) ) 
+   
+
+
+    
--- reflex_trial/state.py
+++ reflex_trial/state.py
@@ -23,6 +23,40 @@ class State(rx.State):
     buffer: str
     quizString: list[str]
     hist: list[tuple[str,str]]
+    quizList: list[tuple[str,str]]
+    quizFlip: bool=False
+    answer: str
+    percent: str
+    given: str
+    score: int
+    quizBuffer: str
+
+    prompts: list[str]
+    promptCount: int
+    count: int
+    gen: str
+    def quizClicked(self):
+        if self.count < 3:
+                self.count +=1  
+                self.generateQuiz()
+                self.genStringQuiz()
+                pd.set_option('display.max_colwidth',None)
+                #self.percent = model.predict( {"text" : f'Give me a percentage between the accuracy of these two answers: ' + {self.given} + ' and '  + {self.answer} })['response'][0]
+                self.quizList.append((self.gen,self.answer))
+
+        else:
+                self.quizFlip=False
+    @rx.var
+    def quizPrompt(self):
+        return self.quizBuffer
+    
+    def genStringQuiz(self):
+        self.gen = self.quizString[self.count].split('/',2)
+        print(self.gen())
+        for i in self.gen[self.count]:
+            self.quizBuffer += i
+            time.sleep(.08)
+            yield
     def log(self):
         request = model.predict({"text": f'{self.boilerPlate}'})
         pd.set_option('display.max_colwidth', None)
@@ -31,37 +65,38 @@ class State(rx.State):
     @rx.var
     def updates(self):
         return self.output
+    
     def generalizationFilter(self,inp: list[str], out: list[str]=[]):
-
         pd.set_option('display.max_colwidth',None)
         for i in inp:
-            out.append( model.predict( {"text": f'What is the main concept in the question: do not repeat the actual question and do not use examples '  + i })['response'][0])
-    
+            out.append( model.predict( {"text": f'what is the topic being discussed in the question: '  + i  + 'your response should not repeat the actual question and should not use examples '})['response'][0])
         return out
+    
     def generateQuiz(self):
-        prompts = self.generalizationFilter(self.quizString)
-        self.quizString.clear()
-        for i in prompts:
-            self.quizString.append(model.predict( {"text": f'Create a question with answer seperated by a \  on the topic of ' + i + "it should be answerable within 10 words" } )['response'][0])
-        
+        self.quizString.append(model.predict( {"text": f'Create a question with answer, seperated with a / on the topic of ' + self.prompts[self.count] + "it should be answerable within 10 words" } )['response'][0])
+        self.given = ( self.quizString[self.count].rsplit( "/",2 ) )
 
     def generate(self):
         self.output = "" 
         if self.iter > 0:
-            self.hist.append((self.outputList[self.iter -1], self.quizString[self.iter -1]))
+            self.hist.append((self.outputList[self.iter-1], self.quizString[self.iter -1]))
         if  (self.iter == 2):
+            self.quizFlip=True
             self.iter =0
-            self.generalizationFilter(self.quizString)
+            self.prompts = self.generalizationFilter(self.quizString)
             self.generateQuiz()
+            self.genStringQuiz()
+            self.quizList.append((self.quizBuffer, self.answer))
+            print(self.buffer)
         else:
             self.boilerPlate = "Explain like im " + self.age + "what " + self.query
-        self.log()
-        self.outputList.append(self.buffer)
-        for i in self.buffer:
-            self.output += i
-            time.sleep(.08)
-            yield
-        self.quizString.append(self.query)
-        self.iter += 1
-    pass
+            self.log()
+            self.outputList.append(self.buffer)
+            for i in self.buffer:
+                self.output += i
+                time.sleep(.08)
+                yield
+            self.quizString.append(self.query)
+            self.iter += 1
+        pass
 
